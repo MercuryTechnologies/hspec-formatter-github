d0bb1ee749941618bde6305d687d67b3405eb3e5
parsonsmatt <parsonsmatt@gmail.com>
2023-12-11 13:09:34 -0700

    neat

--- test/Main.hs
+++ test/Main.hs
@@ -9,7 +9,7 @@ main = do
     hspecWith (registerGithubActionsAnnotations checks defaultConfig) $ do
         describe "it works" $ do
             it "unmark pending to test" $ do
-                -- pendingWith "test is pending by default"
+                pendingWith "test is pending by default"
                 expectationFailure "oh no"
 
 
